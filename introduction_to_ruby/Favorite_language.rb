2356ad742467449d60ea027b00a26ed19ba5f710
Валерия <121750918+lerushsh@users.noreply.github.com>
2025-03-27 00:13:18 +0300

    Добавлен ввод и обработка любимого языка

--- introduction_to_ruby/Favorite_language.rb
+++ introduction_to_ruby/Favorite_language.rb
@@ -4,4 +4,31 @@ if ARGV.empty?      # Проверка есть ли входные аргуме
 else                              
   name = ARGV[0]                  # Если есть, то имя = первый аргумент
 end
+ARGV.clear                        # Очищаем ARGV, чтобы gets работал с клавиатурой
 puts "Привет, #{name}" 
+
+puts "Какой твой любимый язык?"
+lang = gets.chomp  
+lang = lang.downcase
+case lang
+when "ruby"
+  puts "Подлиза)"
+when "python"
+  puts "Ну окей..."
+when "c"
+  puts "Ого, сколько тебе лет..?"
+when "c++"
+  puts "Любишь спать с паяльником?)"
+when "r"
+  puts "Давай дружить?)"
+when "c#"
+  puts "Ты случайно не работаешь в CarX?"
+when "java"
+  puts "Круто!"
+else
+  puts "Ой, я такой пока не знаю"
+end
+
+if lang != "ruby"
+  puts "Cкоро будет ruby (лучше подчиниться)"
+end
