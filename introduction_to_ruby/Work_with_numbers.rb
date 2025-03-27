c9adce7a9dd5582444dd5828a835257adfe53604
Валерия <121750918+lerushsh@users.noreply.github.com>
2025-03-27 05:02:01 +0300

    Добавлены функции prod_of_spec_divisors, sum_of_digits
    prod_of_spec_divisors - Подсчет произведения таких делителей числа, сумма цифр которых меньше, чем сумма цифр исходного числа
    sum_of_digits - сумма цифр числа

--- introduction_to_ruby/Work_with_numbers.rb
+++ introduction_to_ruby/Work_with_numbers.rb
@@ -31,7 +31,28 @@ def count_odd_digits_gt_3(number)
   count
 end
 
+# Сумма цифр числа:
+def sum_of_digits(number)
+  sum = 0
+  while number > 0
+    sum += number % 10  
+    number /= 10        
+  end
+  sum
+end
+
+# Произведение таких делителей числа, сумма цифр которых меньше, чем сумма цифр исходного числа:
+def prod_of_spec_divisors(number)
+  main_sum = sum_of_digits(number)
+  prod = 1
+  (1..number).each do |i|
+    prod *= i if number % i == 0 && sum_of_digits(i) < main_sum
+  end
+  prod
+end
+
 puts "Введите число: "
 number = gets.to_i                             
 puts "Сумма простых делителей числа: #{sum_prime_divisors(number)}"
 puts "Количество нечетных цифр числа, больших 3: #{count_odd_digits_gt_3(number)}"
+puts "Произведение таких делителей числа, сумма цифр которых меньше, чем сумма цифр исходного числа: #{prod_of_spec_divisors(number)}"
