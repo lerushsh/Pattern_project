5910f32373b4d09fa3a2cbcfa24f0ee47c2274d4
lerushsh <121750918+lerushsh@users.noreply.github.com>
2025-03-29 20:28:03 +0300

    Добавлен класс Student
    Добавлен инициализатор, геттеры и сеттеры

---
+++ student/Student.rb
@@ -0,0 +1,85 @@
+class Student
+  @@next_id = 1 # Классовая переменная для хранения следующего ID
+  
+  # Конструктор класса
+  def initialize(last_name, first_name, patronymic)
+    # Задаем ID автоматически
+    @id = @@next_id + 1
+    @@next_id += 1 # Для следующего id добавляем +1
+
+    # Задаем ФИО (обязательные параметры)
+    @last_name = last_name # Фамилия 
+    @first_name = first_name # Имя
+    @patronymic = patronymic # Отчетсво 
+    # Необязательные параметры = nil
+    @phone = nil
+    @telegram = nil
+    @email = nil
+    @git = nil
+  end
+
+  # Геттеры
+  def id
+    @id
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def patronymic
+    @patronymic
+  end
+
+  def phone
+    @phone
+  end
+
+  def telegram
+    @telegram
+  end
+
+  def email
+    @email
+  end
+
+  def git
+    @git
+  end
+
+  # Сеттеры
+  def id=(id)
+    @id = id
+  end
+
+  def last_name=(last_name)
+    @last_name = last_name
+  end
+
+  def first_name=(first_name)
+    @first_name = first_name
+  end
+
+  def patronymic=(patronymic)
+    @patronymic = patronymic
+  end
+
+  def phone=(phone)
+    @phone = phone
+  end
+
+  def telegram=(telegram)
+    @telegram = telegram
+  end
+
+  def email=(email)
+    @email = email
+  end
+
+  def git=(git)
+    @git = git
+  end
