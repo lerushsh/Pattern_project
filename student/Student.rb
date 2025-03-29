b83aae692a751e5dd9066540826ff2472f00de62
lerushsh <121750918+lerushsh@users.noreply.github.com>
2025-03-29 23:01:28 +0300

    Добавлены атрибуты для геттеров и сеттеров

--- student/Student.rb
+++ student/Student.rb
@@ -1,6 +1,9 @@
 class Student
   @@next_id = 0 # Классовая переменная для хранения следующего ID
   
+  attr_accessor :last_name, :first_name, :patronymic, :phone, :telegram, :email, :git
+  attr_reader :id # Только геттер для id, чтоб не меняли id
+
   # Конструктор класса
   def initialize(last_name, first_name, patronymic)
     # Задаем ID автоматически
@@ -12,79 +15,10 @@ class Student
     @first_name = first_name # Имя
     @patronymic = patronymic # Отчетсво 
     # Необязательные параметры = nil
-    @phone = nil
-    @telegram = nil
-    @email = nil
-    @git = nil
+    @phone = @telegram = @email = @git = nil
   end
 
-  # Геттеры
-  def id
-    @id
-  end
-
-  def last_name
-    @last_name
-  end
-
-  def first_name
-    @first_name
-  end
-
-  def patronymic
-    @patronymic
-  end
-
-  def phone
-    @phone
-  end
-
-  def telegram
-    @telegram
-  end
-
-  def email
-    @email
-  end
-
-  def git
-    @git
-  end
-
-  # Сеттеры
-  def id=(id)
-    @id = id
-  end
-
-  def last_name=(last_name)
-    @last_name = last_name
-  end
-
-  def first_name=(first_name)
-    @first_name = first_name
-  end
-
-  def patronymic=(patronymic)
-    @patronymic = patronymic
-  end
-
-  def phone=(phone)
-    @phone = phone
-  end
-
-  def telegram=(telegram)
-    @telegram = telegram
-  end
-
-  def email=(email)
-    @email = email
-  end
-
-  def git=(git)
-    @git = git
-  end
-
-  def get_info()
+  def to_s()
     "ID: #{@id}, ФИО: #{@last_name} #{@first_name} #{@patronymic},\n Git: #{@git ? @git : 'нет'},\n Тел: #{@phone ? @phone : 'нет'},\n Телеграм: #{@telegram ? @telegram : 'нет'},\n Почта: #{@email ? @email : 'нет'} \n"
 
   end
