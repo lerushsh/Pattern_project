620063b2b605dc87cfd725518fd9ec97477ab993
lerushsh <121750918+lerushsh@users.noreply.github.com>
2025-04-17 03:17:36 +0300

    Создан класс Student_short

---
+++ student/Student_short.rb
@@ -0,0 +1,39 @@
+class Student_short
+
+  attr_reader :id, :fio, :git, :contact 
+
+  # Конструктор класса из объекта Student
+  def self.initialize_from_student(student)
+    new(
+      id: student.id,
+      fio: "#{student.last_name} #{student.first_name[0]}.#{student.patronymic[0]}.",
+      git: student.git,
+      contact: student.contact_info == 'Нет контакта' ? nil : student.contact_info
+    )
+  end
+
+  def self.initialize_from_string(id, info_string)
+    fio, git_part, contact = info_string.split(', ')
+  
+    new(
+      id: id,
+      fio: fio,
+      git: (git_part.sub('Git: ', '') unless git_part.sub('Git: ', '') == 'нет'), 
+      contact: (contact unless contact == 'Нет контакта')
+    )
+  end
+
+  def to_s()
+    "ID: #{@id}, ФИО: #{@fio}, Git: #{@git ? @git : 'нет'}, Контакт: #{@contact ? @contact : 'нет'} \n"
+  end
+
+  private_class_method :new
+
+  def initialize(id:, fio:, git:, contact:)
+    @id = id
+    @fio = fio
+    @git = git
+    @contact = contact
+  end
+
+end
--- student/main.rb
+++ student/main.rb
@@ -1,4 +1,5 @@
 require_relative 'Student'
+require_relative 'Student_short'
 
 student1 = Student.new(
   last_name: "Мишенькина", 
@@ -24,16 +25,22 @@ student3 = Student.new(
   telegram: "@alex_sidorov",
   git: "https://github.com/sidorov"
 )
+student_short1 = Student_short.initialize_from_student(student1) 
+student_short2 = Student_short.initialize_from_student(student2) 
+student_short3 = Student_short.initialize_from_student(student3)
+
+student_short4 = Student_short.initialize_from_string(4, student1.getInfo) 
+student_short5 = Student_short.initialize_from_string(5, student2.getInfo) 
+student_short6 = Student_short.initialize_from_string(6, student3.getInfo) 
 
 # Выводим информацию о студентах
 puts "Cтуденты:"
-puts student1.getInfo
-puts student2.getInfo
-puts student3.getInfo
-
-puts student1.validate?
-puts student2.validate?
-puts student3.validate?
+puts student_short1.to_s
+puts student_short2.to_s
+puts student_short3.to_s
+puts student_short4.to_s
+puts student_short5.to_s
+puts student_short6.to_s
 
 student4 = Student.create_from_string("Имя: Леша, Фамилия: Сидородов, Отчество: Андреевич, гит: https://github.com/sidorov")
 puts student4.to_s
